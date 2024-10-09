0676909161593f3c60dcbd63cf5df669f2af12bc
MAC <mac@MacBook-Pro.local>
2024-10-09 07:34:08 +0100

    rfguvdhfvdvfgbfhfgh

--- src/trustgain/pages/admin/index.js
+++ src/trustgain/pages/admin/index.js
@@ -44,7 +44,7 @@ function AdminIndex(props) {
       <AppBar position="fixed">
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
-           Trustcoin admin
+           Keplaget admin
           </Typography>
         </Toolbar>
       </AppBar>
